f802bab1f0d9b204bec7733cbdb1ce7d95e6c886
nagarajRPoojari <np137270@gmail.com>
2025-08-05 22:26:48 +0530

    setup leader lease object

---
+++ internal/utils/env.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"os"
+
+	"github.com/nagarajRPoojari/orange/parrot/utils/log"
+)
+
+func GetEnv(key string, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		log.Warnf("%s =`%s`", key, fallback)
+		return value
+	}
+	log.Warnf("%s not set, using `%s`", key, fallback)
+	return fallback
+}
