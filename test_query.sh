f67c606a074683c77b46af9cfea65afc8df6f071
nagarajRPoojari <np137270@gmail.com>
2025-08-11 13:59:17 +0530

    update submodule reference

--- test_query.sh
+++ test_query.sh
@@ -3,19 +3,19 @@
 echo "creating a schema"
 curl -X POST http://localhost:8000/  \
      -H "Content-Type: application/json" \
-     -d '{"query": "create document item {\"name\":\"STRING\"}"}'
+     -d '{"query": "create document test {\"name\":\"STRING\"}"}'
 
-echo "Inserting documents from ID 1 to 10..."
+echo "Inserting documents from ID 1 to 20..."
 
-for i in $(seq 1 2); do
+for i in $(seq 1 20); do
   curl -s -X POST http://localhost:8000/ \
        -H "Content-Type: application/json" \
-       -d "{\"query\": \"insert value into item  {\\\"_ID\\\": $i, \\\"name\\\": \\\"hello-$i\\\"}\"}"
+       -d "{\"query\": \"insert value into test  {\\\"_ID\\\": $i, \\\"name\\\": \\\"hello-$i\\\"}\"}"
 done
 
 echo "Done."
 
-echo "search a sample doc with id = 4"
+echo "search a sample doc with id = 13"
 curl -X POST http://localhost:8000/  \
      -H "Content-Type: application/json" \
-     -d '{"query": "select * from item where _ID = 1"}'
+     -d '{"query": "select * from test where _ID = 13"}'
